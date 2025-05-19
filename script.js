dc9ddf443c586650bcc0d9e08ad949347a60aa7d
Fejanos <fenyvesijani97@gmail.com>
2025-05-19 10:09:44 +0200

    inner carouselhez fűzés

--- script.js
+++ script.js
@@ -45,7 +45,31 @@ fetch('https://api.sampleapis.com/coffee/hot')
             // <img> elem
             const img = document.createElement('img');
             img.src = item.image; // kép URL az API válaszból
+            img.className = 'd-block w-100';
             img.alt = item.title; // alternatív szöveg a képhez
+
+            // új div, ami cím és leírás
+            const caption = document.createElement('div');
+            caption.className = 'carousel-caption d-none d-md-block';
+
+            // új elem egy címsor <h5>
+            const title = document.createElement('h5');
+            title.textContent = item.title;
+
+            // p - leírás - kávé leírása
+            const paragraph = document.createElement('p');
+            paragraph.textContent = item.description;
+
+            // cím, leírás hozzáadása a caption div-hez
+            caption.appendChild(title);
+            caption.appendChild(paragraph);
+
+            // kép, caption hozzáadása a slide-hoz
+            carouselItem.appendChild(img);
+            carouselItem.appendChild(caption);
+
+            // kész slide hozzáadása a carousel belső tartalmához
+            inner.appendChild(carouselItem);
         })
     })
     .catch(error => {
